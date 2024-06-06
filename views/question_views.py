0b28d7adb23b3c816ad2effb522a5a1a2d8332f3
twoweeksbro <twoweeksbro@gmail.com>
2024-06-06 21:38:46 +0900

    form module for data verification

--- views/question_views.py
+++ views/question_views.py
@@ -6,7 +6,7 @@ from werkzeug.utils import redirect
 from hello import db
 
 from hello.models import Question
-from hello.forms import QuestionForm
+from hello.forms import QuestionForm, AnswerForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -19,8 +19,9 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=('GET','POST'))
 def create():
